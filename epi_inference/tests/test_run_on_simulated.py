d2e7e8f63959ae9563a349ef002382a999e5b114
Carl Laird <carldlaird@users.noreply.github.com>
2020-06-23 00:22:12 -0500

    updates to the simulation end-to-end tests

--- epi_inference/tests/test_run_on_simulated.py
+++ epi_inference/tests/test_run_on_simulated.py
@@ -154,7 +154,7 @@ class TestRunOnSimulated():
         #
         # compare all the json files (gold standard)
         #
-        for f in results_json_files():
+        for f in results_json_files(seeds):
             output_file = os.path.join('results', f)
             baseline_file = os.path.join('gold_results', f) 
             compare_json(output_file, baseline_file, abs_tol=1e-6)
@@ -162,46 +162,20 @@ class TestRunOnSimulated():
         #
         # compare all the csv files (gold standard)
         #
-        for f in results_csv_files():
+        for f in results_csv_files(seeds):
             output_file = os.path.join('results', f)
             baseline_file = os.path.join('gold_results', f) 
             compare_csv(output_file, baseline_file)
 
-def results_json_files():
-    return ['run_on_simulated/recon/recon_deterministic_delay_10.json',
-            'run_on_simulated/recon/recon_stochastic_10_1236482.json',
-            'run_on_simulated/recon/recon_stochastic_10_2800890.json',
-            'run_on_simulated/recon/recon_stochastic_10_2840373.json',
-            'run_on_simulated/recon/recon_stochastic_10_3116341.json',
-            'run_on_simulated/recon/recon_stochastic_10_4995435.json',
-            'run_on_simulated/recon/recon_stochastic_10_6742372.json',
-            'run_on_simulated/recon/recon_stochastic_10_7549469.json',
-            'run_on_simulated/recon/recon_stochastic_10_8334300.json',
-            'run_on_simulated/recon/recon_stochastic_10_8573365.json',
-            'run_on_simulated/recon/recon_stochastic_10_9797259.json',
-            'run_on_simulated/inference/inference_mobility_window_10_1236482.json',
-            'run_on_simulated/inference/inference_mobility_window_10_2800890.json',
-            'run_on_simulated/inference/inference_mobility_window_10_2840373.json',
-            'run_on_simulated/inference/inference_mobility_window_10_3116341.json',
-            'run_on_simulated/inference/inference_mobility_window_10_4995435.json',
-            'run_on_simulated/inference/inference_mobility_window_10_6742372.json',
-            'run_on_simulated/inference/inference_mobility_window_10_7549469.json',
-            'run_on_simulated/inference/inference_mobility_window_10_8334300.json',
-            'run_on_simulated/inference/inference_mobility_window_10_8573365.json',
-            'run_on_simulated/inference/inference_mobility_window_10_9797259.json',
-            'run_on_simulated/inference/inference_mobility_window_unsampled_10.json']
+def results_json_files(seeds):
+    ret = ['run_on_simulated/recon/recon_deterministic_delay_10.json', 'run_on_simulated/inference/inference_mobility_window_unsampled_10.json']
+    ret.extend(['run_on_simulated/recon/recon_stochastic_10_{}.json'.format(sd) for sd in seeds])
+    ret.extend(['run_on_simulated/inference/inference_mobility_window_10_{}.json'.format(sd) for sd in seeds])
+    return ret
 
-def results_csv_files():
-    return ['run_on_simulated/recon/recon_stochastic_summary_10.csv',
-            'run_on_simulated/recon/recon_deterministic_delay_10.csv',
-            'run_on_simulated/recon/recon_stochastic_10.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_1236482.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_2800890.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_2840373.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_3116341.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_4995435.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_6742372.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_7549469.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_8334300.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_8573365.csv',
-            'run_on_simulated/inference/inference_mobility_window_10_9797259.csv']
+def results_csv_files(seeds):
+    ret = ['run_on_simulated/recon/recon_stochastic_summary_10.csv',
+           'run_on_simulated/recon/recon_deterministic_delay_10.csv',
+           'run_on_simulated/recon/recon_stochastic_10.csv']
+    ret.extend(['run_on_simulated/inference/inference_mobility_window_10_{}.csv'.format(sd) for sd in seeds])
+    return ret
